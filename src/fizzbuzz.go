e5d8f18fd32e56bedb6ff29af51b0298a79f5bb9
Miguel Savignano <migue.masx@gmail.com>
2018-06-07 00:39:22 +0200

    fizzbuzz

--- src/fizzbuzz.go
+++ src/fizzbuzz.go
@@ -2,17 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
-  for i := 1; i <= 100; i++ {
-    var word = ""
-    if i % 3 == 0 {
+func fizzbuzz(size int) {
+	for i := 1; i <= size; i++ {
+		var word = ""
+		if i % 3 == 0 {
 			word += "Fizz"
-    } else if i % 5 == 0 {
+		} else if i % 5 == 0 {
 			word += "Buzz"
 		} else {
 			word = fmt.Sprintf("%v",i)
 		}
 		fmt.Println(word)
 		word = ""
-  }
+	}
+}
+
+func main() {
+	fizzbuzz(100)
 }
