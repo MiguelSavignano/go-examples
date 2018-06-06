f221802cd05ef37941a17af769e8a562e16bedff
Miguel Savignano <migue.masx@gmail.com>
2018-06-07 01:34:58 +0200

    more complex boom clousure

--- src/fizzbuzzboom.go
+++ src/fizzbuzzboom.go
@@ -1,12 +1,14 @@
 package main
 
-// import "fmt"
+import "strings"
+import "fmt"
 import (
 	L "./fizzbuzz/lib"
 )
 func main() {
 	boom_word_clousure := func(number int) string {
-		return "Boom"
+		var o_repeats = strings.Repeat("o", number)
+		return fmt.Sprintf("B%sm!!", o_repeats)
 	}
 
 	L.Fizzbuzz(100, boom_word_clousure)
